a2e890cfe90570b5f93f973f563c6faed8affe80
nicetomina <nicetomina@gmail.com>
2024-03-27 20:01:54 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Login from "./Page/Login";
+import ProductAll from "./Page/ProductAll";
+import ProductDetail from "./Page/ProductDetail";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+
+//1. [페이지] 전체상품페이지, 로그인페이지, 상품상세페이지
+//1-1. [네브바]
+//2. [로그인] 로그인 버튼 클릭시/ 상품클릭했을때 로그인 안되어있을 경우
+//3. 로그아웃 버튼 누르면 로그아웃, 로그인<>로그아웃 버튼 변경
+//4. [검색] 상품을 검색할 수 있음
+
+function App() {
+  return (
+    <div>
+      <Navbar/> 
+        <Routes>
+          <Route path="/" element={<ProductAll/>}/> 
+          <Route path="/login" element={<Login/>}/> 
+          <Route path="/product/:id" element={<ProductDetail/>}/> 
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  // 메뉴 배열 만들어주기
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M HOME",
+    "Sale",
+    "지속가능성",
+  ];
+
+  return (
+    <div>
+      <div>
+        <div className="login-area">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+
+      <div className="logo-area">
+        <img
+          width={100}
+          src="https://churchillsquare.com/wp-content/uploads/2018/05/h-m.png"
+        />
+      </div>
+
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+        <div className="search-area">
+          <FontAwesomeIcon icon={faSearch} />
+          <input placeholder="제품검색"></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
